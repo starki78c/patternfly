56b2736d1d306c6ceae160bf7c06db6b3cd20ffb
Christian Stark <cstark@cstark.muc.csb>
2017-12-07 13:26:47 +0000

    first commit

---
+++ build/static/js/6.18ac77c2.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([6],{551:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var o=t(0);n.NotFound=function(){return o.createElement("div",{className:"NotFound"},o.createElement("h3",null,"Sorry, page not found!"))}}});
+//# sourceMappingURL=6.18ac77c2.chunk.js.map
